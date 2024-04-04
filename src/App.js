78f54b379b8464fcaa727b43e710937e80375475
Vikas_Shanabhog <119601456+vikas9872@users.noreply.github.com>
2024-04-04 21:42:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Props from './propsConcept'
 function App() {
   return (
     <>
+      {/* Passing single element as a prop */}
       <Props name="Vikas Shanabhog"/>
+      {/* Passing multiple elements as props */}
+      <MultipleElementsasProps name="Vikas" university="Global Academy of Technology"/>
     </>
   );
 }
