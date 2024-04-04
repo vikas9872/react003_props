3c11add158f08d4c3c2ced9f154042539062c6e9
vikas9872 <shanabhogvikas648.com>
2024-04-04 20:21:49 +0530

    propsConcept

---
+++ src/propsConcept.js
@@ -0,0 +1,7 @@
+// Props helps to pass data from parent component to the child component.
+const Props=(props)=>{
+    return(
+        <h1>Name is printed using props concept {props.name}</h1>
+    )
+}
+export default Props;
