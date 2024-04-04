b7e7f2086ed4cec1115eae1201c832e2a6c1267a
Vikas_Shanabhog <119601456+vikas9872@users.noreply.github.com>
2024-04-04 21:44:17 +0530

    Update propsConcept.js

--- src/propsConcept.js
+++ src/propsConcept.js
@@ -1,7 +1,19 @@
 // Props helps to pass data from parent component to the child component.
+
+// Single element as prop
 const Props=(props)=>{
     return(
         <h1>Name is printed using props concept {props.name}</h1>
     )
 }
 export default Props;
+
+// Multiple elements as props
+export const MultipleElementsasProps=(props)=>{
+    return(
+        <>
+            <h1>Name: {props.name}</h1>
+            <h1>University: {props.university}</h1>
+        </>
+    )
+}
